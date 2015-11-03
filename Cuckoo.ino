26475320df11b6524e7ebbb7ce501bee658a4fd9
Gustavo <gustavo.m.netto@outlook.com>
2015-11-02 22:15:12 -0500

    Counting 1 sec but not playing sound correctly

--- Cuckoo.ino
+++ Cuckoo.ino
@@ -2,33 +2,53 @@
 #include "cuckoo.h"
 #include "dingdong.h"
 
+const int led_pin = 13;
+int state = 0;
+int counter = 0;
+int minute_counter = 60;
 
-ISR(TIMER2_COMPA_vect){
+ISR(TIMER1_COMPA_vect){
   //Interrupt action
+  state = 1 - state;
+  digitalWrite(led_pin, state);
+  counter++;
+  Serial.println(counter);
+  if(counter == 10){
+    int i;
+    for(i = 0; i < dingdong_length; i++){
+      tone(11, 8000, cuckoo_data[i]);
+    }
+    counter = 0;
+    Serial.println(state);
+    noTone(11);
+  }
 }
 
 void setup_every_1s(){
-  //I still have to discover what is it for. But TCC is a timer for sure.
-  TCCR2B = _BV(CS21) || _BV(CS20);
+  //Timers to 0
+  TCCR1A = 0;
+  TCCR1B = 0;
 
-  // set WGM to CTC mode (010)
-  // In this mode Timer2 counts up until it matches OCR2A
-  TCCR2A = _BV(WGM21);
+  //Acummulator timer
+  TCNT1 = 0;
 
-  //frequency to interrupt.
-  OCR2A = 1;
-
-  //when timer 2 matches OCR, it interrupts
-  TIMSK2 = _BV(OCIE2A);
+  // compare match register 16MHz/256/2Hz
+  OCR1A = 62500;
+  TCCR1B |= (1 << WGM12);   // CTC mode
+  TCCR1B |= (1 << CS12);    // 256 prescaler 
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt            
 }
 
 void setup() {
   //disable interrupts
-  cli();
+  pinMode(led_pin, OUTPUT);
+  noInterrupts();
   setup_every_1s();
+  
 
   //enable interrrupts
-  sei();
+  interrupts();
+  Serial.begin(9600); 
 }
 
 void loop() {
